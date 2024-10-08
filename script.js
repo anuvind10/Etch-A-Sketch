4679f91c9a0c1231eed2b3b599ac721804e4ac12
Anuvind <119839716+anuvind10@users.noreply.github.com>
2024-10-08 18:28:29 +0530

    add ability to clear the grid

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const CONTAINER = document.querySelector("#container");
 const gridSize = document.querySelector("#gridSize");
+const clearGrid = document.querySelector("#clearGrid");
 const defaultGridSize = 16;
 const containerSize = 650;
+var size = defaultGridSize;
+var red;
+var blue;
+var green;
 
 gridSize.addEventListener("click", resetGrid);
+clearGrid.addEventListener("click", resetGrid)
 
-function resetGrid (){
-    let size = prompt("Enter the desired grid size (max 100):");
-    createGrid(size);
-    colorGrid();
+function resetGrid (event){
+    if (event.target.id == "gridSize") {
+        size = prompt("Enter the desired grid size (max 100):");
+    }
+    createGrid(size == null ? defaultGridSize : size);
 }
 
 function createGrid(size) {
+    // Remove existing grid when resizing the grid.
     while(CONTAINER.hasChildNodes()) {
         CONTAINER.removeChild(CONTAINER.firstChild);
     }
 
+    // Determine size of each square based on the number of squares to fit in the fixed container size.
     let squareSize = containerSize / size;
 
     for (let i = 0; i < size * size; i++) {
@@ -25,6 +34,8 @@ function createGrid(size) {
         square.style.width = `${squareSize}px`  ;
         CONTAINER.appendChild(square);
     }
+
+    colorGrid();
 }
 
 function colorGrid() {
@@ -32,14 +43,25 @@ function colorGrid() {
 
     GRIDS.forEach((grid) => {
         grid.addEventListener("mouseenter", (event) => {
-            var red = Math.floor(Math.random() * 256);
-            var green = Math.floor(Math.random() * 256);
-            var blue = Math.floor(Math.random() * 256);
-            event.target.style.backgroundColor = "rgb("+`${red}`+", "+`${green}`+", "+`${blue}`+")";
+            // new color if it's an uncolored square.
+            if (!event.target.style.backgroundColor){
+                generateRandomColor();
+                event.target.style.backgroundColor = "rgba("+`${red}`+", "+`${green}`+", "+`${blue}`+")";
+                event.target.style.opacity = 0.1;
+            }
+            // increase opacity if mouse passes over an already colored square.
+            else if (event.target.style.opacity < 1){
+                event.target.style.opacity = parseFloat(event.target.style.opacity) + 0.1;
+            }
         });
     });
 }
 
-createGrid(defaultGridSize);
-colorGrid();
+function generateRandomColor(){
+    red = Math.floor(Math.random() * 256);
+    blue = Math.floor(Math.random() * 256);
+    green = Math.floor(Math.random() * 256);
+}
+
+createGrid(size);
 
